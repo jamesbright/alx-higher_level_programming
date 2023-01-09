efd75a8396be06512cb1c9f2b1122fcc491ea44b
James Ononiwu <jamesononiwu@gmail.com>
2023-01-09 06:54:42 -0800

    replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    new_list = my_list[0:]
+
+    if idx < 0:
+        return new_list
+    elif idx >= len(new_list):
+        return new_list
+    else
+    new_list[idx] = element
+    return new_list
