b5944abfcfeb18367eb847ad7cdb16de3f8eded3
James Ononiwu <jamesononiwu@gmail.com>
2023-01-09 06:00:40 -0800

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for index in my_list:
-        print("{:d}".format(my_list[index]))
+    for value in my_list:
+        print("{:d}".format(value))
