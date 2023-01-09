3683e6a91e6396dba3d6ff15897198b432a03e6a
James Ononiwu <jamesononiwu@gmail.com>
2023-01-09 06:27:56 -0800

    print in reversed

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    if len(my_list) > 0:
+        index = len(my_list) - 1
+        while index >= 0:
+            print("{:d}".format(my_list[index]))
+            index -= 1
