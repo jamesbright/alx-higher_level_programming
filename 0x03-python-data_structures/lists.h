4d98b8bc5c69b303b83c5b89f8b50090b27d02f0
James Ononiwu <jamesononiwu@gmail.com>
2023-01-10 15:52:00 -0800

    update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 int  print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
