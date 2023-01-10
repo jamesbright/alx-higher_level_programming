22bac5632277557c91cf98513b85f9b20f2e0485
James Ononiwu <jamesononiwu@gmail.com>
2023-01-10 12:50:29 -0800

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints elements of a 
+    """Prints elements of a
     matrix
     """
-    if not matrix:
+    if matrix == [[]]:
         print()
     else:
-        
         for row in matrix:
             for col in row:
                 print("{:d}".format(col), end=' ' if col != row[-1] else '\n')
