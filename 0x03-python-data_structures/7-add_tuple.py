5f5a358ff7e7c15b368846b4b14da34d600d2cf7
James Ononiwu <jamesononiwu@gmail.com>
2023-01-10 13:06:30 -0800

    added len function for correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,9 +4,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
 
-    while list_a < 2:
+    while len(list_a) < 2:
         list_a.append(0)
-    while list_b < 2:
+    while len(list_b) < 2:
         list_b.append(0)
 
     res = tuple([list_a[0] + list_b[0], list_a[1] + list_b[1]])
