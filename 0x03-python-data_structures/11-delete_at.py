6704435bdc7d293dd6ba34c8c288f8bd477092f7
James Ononiwu <jamesononiwu@gmail.com>
2023-01-10 15:03:26 -0800

    delete element at index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return (my_list)
+    else:
+        new_list = list()
+        for index in range(len(my_list)):
+            if index == idx:
+                continue
+            new_list[index] = my_list[index]
+        return (new_list)
