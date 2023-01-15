95b349b77015dfbd4345467fb1b11bac6446754d
James Ononiwu <jamesononiwu@gmail.com>
2023-01-15 05:19:06 -0800

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,12 @@
 
 def square_matrix_simple(matrix=[]):
     """finds the squares of all elements
-    of a matrix"""7
-    square = [i ** 2 for i in matrix]
+    of a matrix"""
+    value = []
 
-    return (square)
+    for row in matrix:
+        square = map(lambda el: el**2, row)
+    
+    value.append(list(square))
+
+    return (value)
