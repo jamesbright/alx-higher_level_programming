65e9569b752fc19d96a1722c2bd0bdb01a7be472
James Ononiwu <jamesononiwu@gmail.com>
2023-01-15 05:54:52 -0800

    updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """finds and replaces a value in a list"""
-    value  = [num if num != search else replace for num in my_list]
+    """finds an7d replaces a value in a list"""
+    value = [num if num != search else replace for num in my_list]
     return value
