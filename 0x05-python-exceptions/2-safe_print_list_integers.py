c2ac7e068044dc9ce42ffa139859dfa1947bc5fc
James Ononiwu <jamesononiwu@gmail.com>
2023-01-24 03:36:10 -0800

    print list integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+
+    for item in range(0, x):
+        try:
+            print("{:d}".format(my_list[item], end=""))
+        except Exception as e:
+            continue
+        else:
+            counter += 1
+
+    print()
+    return counter
