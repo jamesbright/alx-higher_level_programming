4aeae59897452f7ca0ce57d944154a3ad729b8e2
James Ononiwu <jamesononiwu@gmail.com>
2023-01-24 04:12:51 -0800

    update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         res = a / b
     except ZeroDivisionError:
-        result = None
+        res = None
     finally:
         print("Inside Result: {}".format(res))
         return res
