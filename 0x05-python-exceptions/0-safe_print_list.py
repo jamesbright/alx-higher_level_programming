e36faebd160356c86945d970fc0b5cf46b2e6264
James Ononiwu <jamesononiwu@gmail.com>
2023-01-24 03:08:00 -0800

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/pythoin3
 
 def safe_print_list(my_list=[], x=0):
     """Prints a list and hanles exception
     with try and catch"""
-
-    counter = 0;
-
-    for item in range(0, x):
+    counter = 0
+    for i in range(0, x):
         try:
-            print("{} ".format(my_list[item], end="")
-        except  Exception as e:
-            break;
+            print("{}".format(my_list[i]), end="")
+        except Exception as e:
+            break
         else:
             counter += 1
-
     print()
-    return counter
-        
+    return count
 
