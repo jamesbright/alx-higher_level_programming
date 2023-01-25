95742a31d991c74d0367089276815fd9c725482e
James Ononiwu <jamesononiwu@gmail.com>
2023-01-25 11:48:06 -0800

    first commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class and functions"""
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes size"""
+        self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if type(size) < 0:
+            raise ValueError("size must be >= 0")
