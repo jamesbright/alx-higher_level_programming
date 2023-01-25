db35f0b31bb19f7159b09162567b98d0d4ef358b
James Ononiwu <jamesononiwu@gmail.com>
2023-01-25 13:50:47 -0800

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""Square class and methods that returns area"""
-
-
 class Square:
     """Defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes size and position"""
+        """Initialises the data"""
         self.size = size
         self.position = position
 
     def area(self):
-        """function returns area data
-        Returns:
-            area: area data
-        """
+        """Returns current square area"""
         return self.__size**2
 
     @property
     def size(self):
-        """Getter"""
+        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter"""
+        """Setter method"""
         self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
@@ -31,25 +25,29 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def my_print(self):
-        """prints size with # character"""
+        """Prints the square"""
         if self.__size == 0:
             print()
         else:
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(' ', end='')
                 for j in range(self.__size):
                     print('#', end='')
                 print()
 
     @property
     def position(self):
-        """Getter"""
+        """Getter method"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter"""
+        """Setter method"""
         if type(value) != tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 integers")
-        if any(type(i) != int for i in value or any(j < 0 for j in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if any(type(i) != int for i in value) or any(j < 0 for j in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
