fb93e5c83bf525f81a6b83aad3168f7b16eef705
James Ononiwu <jamesononiwu@gmail.com>
2023-01-25 11:54:59 -0800

    return area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class and functions that returns area"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes size"""
+        self.__size = size
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """function returns area data
+        Returns:
+            area: area data
+        """
+        return self.__size**2
