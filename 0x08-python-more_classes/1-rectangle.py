4494a5d7b63e4bd00ea2ea036fbff10074539264
James Ononiwu <jamesononiwu@gmail.com>
2023-01-31 03:40:39 -0800

    hot fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Rectangle class and methods"""
+"""Rectangle class and methods with attribute width and height"""
 
 
 class Rectangle:
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialise Objects"""
-        this.width = width
-        this.height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
