bcbd44fe7d694e4ca5755f2830160efa409d296e
James Ononiwu <jamesononiwu@gmail.com>
2023-01-31 03:54:34 -0800

    area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Rectangle class and methods with attribute width and height"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initialise Objects"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width property"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height property"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate and return area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculate and return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)                            
