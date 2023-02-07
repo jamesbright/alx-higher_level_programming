9076d881dba641ded8e778829026430f7eaca080
James Ononiwu <jamesononiwu@gmail.com>
2023-02-07 09:55:20 -0800

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-Line function
-"""
 
 
 def number_of_lines(filename=""):
-    """number of lines from file
-    args:
-        filename: file to read
-    return:
-        number of lines
-    """
     with open(filename, "r", encoding="UTF-8") as f:
         return len(list(f))
