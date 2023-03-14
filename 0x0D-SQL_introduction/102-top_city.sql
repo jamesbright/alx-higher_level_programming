3d5586729ebeadd2ecd2abb20729c68415ede422
James Ononiwu <jamesononiwu@gmail.com>
2023-03-14 14:20:36 -0700

    changed MAX to AVG

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- displays top 3 city temperatures
-SELECT `city`, MAX(`value`) AS `top_temp`
+SELECT `city`, AVG(`value`) AS `top_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
