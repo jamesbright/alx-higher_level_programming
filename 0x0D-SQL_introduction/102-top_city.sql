6d5e5395e668620f59c8d12a8826e0e585693901
James Ononiwu <jamesononiwu@gmail.com>
2023-03-14 14:17:24 -0700

    top temp

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displays top 3 city temperatures
+SELECT `city` MAX(`value`) AS `top_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `top_temp` DESC
+LIMIT 3;
