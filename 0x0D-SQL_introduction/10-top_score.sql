30ff61bf3e3e67c6d8becf7d02941325cb4f51e0
James Ononiwu <jamesononiwu@gmail.com>
2023-03-14 13:18:41 -0700

    removed errors

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,2 @@
 -- list all records of `second_table` in a sorted order
-SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC`:WQ
-;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
