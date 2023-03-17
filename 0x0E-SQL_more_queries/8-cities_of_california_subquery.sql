adc7cff66b878d8f63df6ec829c95bb09465a361
James Ononiwu <jamesononiwu@gmail.com>
2023-03-17 02:14:31 -0700

    added mandatory tasks

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- list cities of california
+-- selects cities where state_id corresponds to name "California"
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
