c4a1c617e261a993e83ac8c125026ecb444915f1
James Ononiwu <jamesononiwu@gmail.com>
2023-04-12 09:20:29 -0700

    comcom

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (isNaN(argv[2])) {
+  console.log('Missing size');
+} else {
+  let i = argv[2];
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
+  }
+}
