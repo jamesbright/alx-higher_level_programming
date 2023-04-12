309e2761efed254168775b4e597cbb1c87e5c946
James Ononiwu <jamesononiwu@gmail.com>
2023-04-12 07:04:35 -0700

    fixed error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (!argv[2]) {
-  console.log('No argument');
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log('No argument');
 }
