10f8fee61b150c45cfdb4a9b1e9bfa6e8e8ba47e
James Ononiwu <jamesononiwu@gmail.com>
2023-04-13 07:48:22 -0700

    hot fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@
 const fs = require('fs');
 const firstFile = fs.readFileSync(process.argv[2]).toString();
 const secondFile = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(processargv[4], firstFile + secondFile);
+fs.writeFileSync(process.argv[4], firstFile + secondFile);
