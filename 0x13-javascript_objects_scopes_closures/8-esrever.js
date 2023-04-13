4dc7f85427b533b7c90c1712fd79ab3156bd5721
James Ononiwu <jamesononiwu@gmail.com>
2023-04-13 06:50:29 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//returns a reversed list
+// returns a reversed list
 exports.esrever = function (list) {
-let i = 0;
-const list2 = list.slice();
-while (i < list.length) {
-list[i] = list2[list.length - i - 1];
-i++;
-}
-return list;
-}
+  let i = 0;
+  const list2 = list.slice();
+  while (i < list.length) {
+    list[i] = list2[list.length - i - 1];
+    i++;
+  }
+  return list;
+};
