35fe1f7943cfd61db18e488c1ed92453ac0ce15e
James Ononiwu <jamesononiwu@gmail.com>
2023-04-18 08:32:31 -0700

    updated

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes in an argument and filters selection"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name='{:s}' ORDER BY
+    states.id".format(argv[4])
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
