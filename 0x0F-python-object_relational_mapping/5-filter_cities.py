96d2e0c706bb679aab3ecf23efc385dd2b7c1c56
James Ononiwu <jamesononiwu@gmail.com>
2023-04-18 13:13:09 -0700

    made corrections

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-     db = MySQLdb.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     sql = "SELECT cities.name FROM cities JOIN states ON\
