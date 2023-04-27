d2d657dc18185c17cd136d846b6b3a5d043a1ead
James Ononiwu <jamesononiwu@gmail.com>
2023-04-27 10:38:52 -0700

    corrected pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,11 @@
 """ Finds the peak of a list
 """
 
+
 def divide_conquer(arr, low, high):
     """ uses divide and conquer algorithm
     """
+
     mid = int((low + high) / 2)
     if arr[mid-1] <= arr[mid] and arr[mid] >= arr[mid+1]:
         return arr[mid]
@@ -13,9 +15,11 @@ def divide_conquer(arr, low, high):
     elif arr[mid] < arr[mid+1]:
         return divide_conquer(arr, mid+1, high)
 
+
 def find_peak(list_of_integers):
     """ find list peak
     """
+
     if not list_of_integers:
         return None
     return divide_conquer(list_of_integers, 0, len(list_of_integers)-1)
