1a7f7a7e08886ba5854d65d31422ef6fff36e505
James Ononiwu <jamesononiwu@gmail.com>
2023-04-28 08:28:36 -0700

    status with requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetch a url with requests
+"""
+
+if __name__ == "__main__":
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
