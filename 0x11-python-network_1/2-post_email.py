22ff2a4c5be8fd75de67e2d35a4ca2f14febe746
James Ononiwu <jamesononiwu@gmail.com>
2023-04-28 07:34:21 -0700

    send email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" make a post request and display body of response
+"""
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    from sys import argv
+
+    url = argv[1]
+    val = {}
+    val['email'] = argv[2]
+    data = urlencode(val)
+    data = data.encode('utf-8')
+
+
+    response = Request(url, data)
+    with urlopen(response) as res:
+        page = res.read().decode('utf-8')
+        print(page)
