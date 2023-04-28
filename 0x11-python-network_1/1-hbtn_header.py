147e3e620edcf0192320c4940bc8b3db9d867527
James Ononiwu <jamesononiwu@gmail.com>
2023-04-28 06:59:44 -0700

    add display header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" fetch and display X-request-Id
+"""
+
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from sys import argv
+
+    response = Request(argv[1])
+    with urlopen(response) as res:
+        xrid_header = res.info()
+        print(xrid_header['X-Request-Id'])
