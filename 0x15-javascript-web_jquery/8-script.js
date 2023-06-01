2228b525772b9ccaacc5f73703f1b3c3bf6b1e28
jamesbright <jamesononiwu@gmail.com>
2023-06-01 01:42:52 -0700

    get movie title

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+t $ = window.$;
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+for (let i = 0; i <= data.results.length; i++) {
+$('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+}
+});
